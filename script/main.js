7557ff089fce06e315f2469aafe7fd3e64d4214a
Noah Loomans <noahloomans@gmail.com>
2016-04-09 20:31:38 +0200

    added self typing text

--- script/main.js
+++ script/main.js
@@ -11,3 +11,42 @@ $(document).ready(function () {
     $('body').removeClass('noscroll');
   });
 })
+
+var data = {
+  thing: 0,
+  letter: 0,
+  thingsYouCanLearn: [
+    'build a robot',
+    'make a website',
+    'develop a game',
+    'do anything.'
+  ]
+}
+
+function type () {
+  $('.what-to-lean').find('strong').text(data.thingsYouCanLearn[data.thing].slice(0,data.letter+1));
+  data.letter++;
+  if (data.letter == data.thingsYouCanLearn[data.thing].length) {
+    if (data.thing == data.thingsYouCanLearn.length-1) {
+      return;
+    }
+    data.thing++;
+    data.letter = 0;
+    setTimeout(type, 2000);
+  }
+  else {
+    setTimeout(type, 50);
+  }
+}
+
+$(document).ready(function () {
+  var thingsYouCanLearn = [
+    'build a robot',
+    'make a website',
+    'develop a game',
+    'anything.'
+  ];
+  type();
+
+  $('.what-to-lean')
+});
